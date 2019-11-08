63428462bbcf6e33d3873e91a9ca807cef354e41
CGI_jonathas <jonathas.mendesalves@cgi.com>
2019-11-08 15:46:36 -0500

    Strang behaviour fixed

--- src/main/java/com/jonathas/concurrency/App.java
+++ src/main/java/com/jonathas/concurrency/App.java
@@ -8,13 +8,17 @@ public class App
 {
     public static void main( String[] args ) throws InterruptedException {
         Counter counter = new Counter();
-        CounterRunnable c1 = new CounterRunnable(counter, "T1");
-        CounterRunnable c2 = new CounterRunnable(counter, "T2");
-        Thread thread1 = new Thread(c1);
-        Thread thread2 = new Thread(c2);
+        Thread thread1 = new Thread(new CounterRunnable(counter, "T1"));
+        Thread thread2 = new Thread(new CounterRunnable(counter, "T2"));
+        Thread thread3 = new Thread(new CounterRunnable(counter, "T3"));
+
         thread1.start();
         thread2.start();
-        Thread.sleep(4000);
+        thread3.start();
+        while(thread1.isAlive() && thread2.isAlive()) {
+            System.out.println("Main thread will be alive till the child thread is live");
+            Thread.sleep(1000);
+        }
         System.out.println( "Hello World! The value was " + counter.count );
     }
 
@@ -25,22 +29,20 @@ public class App
     public CounterRunnable(Counter counter, String name) { this.counter = counter; this.name = name; }
     public void run() {
         int i = 0;
-        while(i++ < 10) {
-            increment();
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        while(i++ < 1000000) {
+            counter.increment();
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
         }
-    }
-
-    synchronized public void increment() {
-        System.out.println(String.format("Thread %s call count %d", this.name, counter.count));
-        counter.count++;
     }
 }
 
 class Counter {
     public int count;
+    public synchronized void increment() {
+        count++;
+    }
 }
