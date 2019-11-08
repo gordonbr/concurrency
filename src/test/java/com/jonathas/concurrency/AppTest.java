8e30770d53f97b0d0dbb6bff900ba13bd8ef990a
CGI_jonathas <jonathas.mendesalves@cgi.com>
2019-11-08 15:11:28 -0500

    First commit

---
+++ src/test/java/com/jonathas/concurrency/AppTest.java
@@ -0,0 +1,20 @@
+package com.jonathas.concurrency;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
